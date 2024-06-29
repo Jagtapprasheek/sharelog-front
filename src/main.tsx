950b21f885f8ebc1d892c63d6121f39579912dd1
dev <vipul@boostmychild.com>
2024-06-29 13:14:48 +0530

    Merge branch 'master' of github.com:Jagtapprasheek/sharelog-front

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import React from 'react'
 /**
- * React redux imports 
- */
+* React redux imports 
+*/
 import { Provider } from 'react-redux'
 import store from './store'
 
